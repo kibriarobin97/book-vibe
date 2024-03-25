163809c7992b8fc833a1eeb6e239e176839e4cfe
kibriarobin97 <kibriarobin97@gmail.com>
2024-03-25 21:57:06 +0600

    added all needed files

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      
+      <h1 className='text-5xl'>Vite + React</h1>
+      <button className="btn btn-secondary">Secondary</button>
+
+      
+    </>
+  )
+}
+
+export default App
